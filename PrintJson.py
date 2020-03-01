66833cc4cbba13214f194873578e0ef071efc5b6
acheng33 <angiesc2@illinois.edu>
2020-02-29 19:04:55 -0600

    prints first document in plSubmissions collection

--- PrintJson.py
+++ PrintJson.py
@@ -1,6 +1,18 @@
-.vscode/*
-!.vscode/settings.json
-!.vscode/tasks.json
-!.vscode/launch.json
-!.vscode/extensions.json
-*.code-workspace
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+import pprint
+
+load_dotenv()
+
+database_url = os.environ.get("CS125MONGO")
+print(database_url)
+
+client = MongoClient(database_url)
+db = client.Fall2019Clean
+
+collection = db.plSubmissions
+
+first_document = collection.find_one()
+
+print(first_document)
