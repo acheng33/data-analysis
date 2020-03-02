79e1fc26b0d258f94b102bdef1183ab1005228b0
acheng33 <angiesc2@illinois.edu>
2020-03-02 14:49:57 -0600

    writing JSON format into file, more verbose JSON format

--- PrintJson.py
+++ PrintJson.py
@@ -15,8 +15,8 @@ db = client.Fall2019Clean
 #list of collection names
 collection_names = []
 
-#appending to collection name list all collections
-#information over the course of the semester
+# appending to collection name list all collections
+# information over the course of the semester
 collection_names.append(db.best)
 collection_names.append(db.bestChanges)
 collection_names.append(db.bestStats)
@@ -25,26 +25,47 @@ collection_names.append(db.bestStats)
 collection_names.append(db.discourseUsers)
 collection_names.append(db.discourseUserChanges)
 
-plSubmissions_collection = db.jeed
+#information about lab, enrollment, and the class
+collection_names.append(db.enrollment)
+collection_names.append(db.labAttendance)
+collection_names.append(db.people)
+collection_names.append(db.peopleChanges)
+collection_names.append(db.state)
 
-first_document = plSubmissions_collection.find_one()
+#information about lecture
+collection_names.append(db.lectureAttendance)
+collection_names.append(db.lectureSummary)
+collection_names.append(db.sliderChanges)
+collection_names.append(db.jeed)
 
+#information about MPs
+collection_names.append(db.MPGrades)
+collection_names.append(db.gradlegrader)
+collection_names.append(db.intellijlogger)
 
-def print_json_keys(collection):
+#information about PairieLearn
+collection_names.append(db.plAssessments)
+collection_names.append(db.plGrades)
+collection_names.append(db.plInstances)
+collection_names.append(db.plQuestions)
+collection_names.append(db.plSubmissions)
+
+file = open("JSONKeys.txt", "w")
+
+def print_json_keys_to_file(collection):
+    file.write(collection.name)
     first_document = collection.find_one()
     
     for key in first_document:
-        print(key)
+        file.write("\n\t" + key)
         if isinstance(first_document[key], dict) or isinstance(first_document[key], list):
 
             for second_key in first_document[key]:
-                print("\t" + second_key)
-                if (isinstance(first_document[key][second_key], dict) or isinstance(first_document[key][second_key], list)):
+                file.write("\n\t\t" + (str(second_key)))
 
-                    for third_key in first_document[key][second_key]:
-                        print("\t\t" + third_key)
+    file.write("\n\n")
 
-# for field in first_document:
-#     print(field)
+for collection in collection_names:
+    print_json_keys_to_file(collection)
 
-print_json_keys(plSubmissions_collection)
+file.close()
