54d6984f85fbc568ec079bd4d39c90e2023d157f
acheng33 <angiesc2@illinois.edu>
2020-03-03 18:02:57 -0600

    added comments and automated reading through the collections

--- PrintJson.py
+++ PrintJson.py
@@ -1,75 +1,59 @@
-from pymongo import MongoClient
-from dotenv import load_dotenv
-
+import pymongo
 import os
 import json
+
+from dotenv import load_dotenv
 
 #load in environment variables
 load_dotenv()
 database_url = os.environ.get("CS125MONGO")
 
 #connect to the mongo database
-client = MongoClient(database_url)
+client = pymongo.MongoClient(database_url)
 db = client.Fall2019Clean
 
-#list of collection names
-collection_names = []
+#creates list of collection names and sorts
+collection_names = db.list_collection_names()
+collection_names.sort()
 
-# appending to collection name list all collections
-# information over the course of the semester
-collection_names.append(db.best)
-collection_names.append(db.bestChanges)
-collection_names.append(db.bestStats)
-
-#information from discourse
-collection_names.append(db.discourseUsers)
-collection_names.append(db.discourseUserChanges)
-
-#information about lab, enrollment, and the class
-collection_names.append(db.enrollment)
-collection_names.append(db.labAttendance)
-collection_names.append(db.people)
-collection_names.append(db.peopleChanges)
-collection_names.append(db.state)
-
-#information about lecture
-collection_names.append(db.lectureAttendance)
-collection_names.append(db.lectureSummary)
-collection_names.append(db.sliderChanges)
-collection_names.append(db.jeed)
-
-#information about MPs
-collection_names.append(db.MPGrades)
-collection_names.append(db.gradlegrader)
-collection_names.append(db.intellijlogger)
-
-#information about PairieLearn
-collection_names.append(db.plAssessments)
-collection_names.append(db.plGrades)
-collection_names.append(db.plInstances)
-collection_names.append(db.plQuestions)
-collection_names.append(db.plSubmissions)
-
+#opens the file to write json keys into
 file = open("JSONKeys.txt", "w")
 
-def print_json_keys_to_file(collection):
-    file.write(collection.name)
-    first_document = collection.find_one()
-    
-    for key in first_document:
-        file.write("\n\t" + key)
-        if isinstance(first_document[key], dict):
+#function which takes in a list of names of collections and writes the keys of the json in each collection to a file
+#param (collections) a list of names of collections which to find the json keys for
+def print_json_keys_to_file(collections):
+    #keeps track of the index of the current collection
+    counter = 0
+   
+    #loops through all the collections
+    for collection in collections:
+        #finds collection from database
+        current_collection = pymongo.collection.Collection(db, collections[counter], create = False)
+        counter += 1
+        
+        #writes the name of 
+        file.write(current_collection.name)
+        first_document = current_collection.find_one()
+        
+        #loops through the document and writes the json keys to previously specified file
+        for key in first_document:
+            file.write("\n\t" + key)
+            if isinstance(first_document[key], dict):
 
-            for second_key in first_document[key]:
-                file.write("\n\t\t" + second_key)
-                if isinstance(first_document[key][second_key], dict):
-                    
-                    for third_key in first_document[key][second_key]:
-                        file.write("\n\t\t\t" + third_key)
+                for second_key in first_document[key]:
+                    file.write("\n\t\t" + second_key)
+                    if isinstance(first_document[key][second_key], dict):
+                        
+                        for third_key in first_document[key][second_key]:
+                            file.write("\n\t\t\t" + third_key)
+                            if isinstance(first_document[key][second_key][third_key], dict):
+                                
+                                for fourth_key in first_document[key][second_key][third_key]:
+                                    file.write("\n\t\t\t\t" + fourth_key)
 
-    file.write("\n\n")
+        file.write("\n\n")
 
-for collection in collection_names:
-    print_json_keys_to_file(collection)
+print_json_keys_to_file(collection_names)
 
+#closes file
 file.close()
