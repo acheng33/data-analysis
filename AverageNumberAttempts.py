f895989b7dc2f1ffd8d920941bf6a4ea1e2eb1ce
acheng33 <angiesc2@illinois.edu>
2020-04-19 00:41:00 -0700

    added method which creates dictionary of question name to average number of attempts per student

--- AverageNumberAttempts.py
+++ AverageNumberAttempts.py
@@ -3,6 +3,7 @@ import os
 import json
 
 from dotenv import load_dotenv
+from CreateDictionary import create_dictionary
 
 #loads environment variables and connects to database
 load_dotenv()
@@ -10,9 +11,33 @@ database_url = os.environ.get("CS125MONGO")
 client = pymongo.MongoClient(database_url)
 db = client.Fall2019Clean
 
-#given an assignment number or assessment number, function will return the average number of tries per student for the question
-#field in plQuestions: ID, field in plSubmissions: question_name
+full_dictionary = create_dictionary()
+
+#sets up the collection which holds the id for each question in labs, homeworks, and exams
 question_collection = db.plQuestions
-submission_collection = db.plSubmissions
 
+def get_number_of_attempts():
+    assignment_to_average_attempts = {}
 
+    #iterates through each question to get the id
+    for question in question_collection:
+        question_id = question['ID']
+        people_to_attempts = {}
+        total_attempts = 0
+
+        #for each question, associate people to number of tries they took 
+        for person in full_dictionary:
+            if question_id in full_dictionary[person]:
+                people_to_attempts.update({person : len(full_dictionary[person][question_id])})
+
+        #iterate through created dictionary to get the total number of attempts made by everyone
+        for person in people_to_attempts:
+            total_attempts += people_to_attempts[person]
+        
+        #updates the assignment to average dictionary by dividing the total_attempts calculated above by the people who have attempted the question
+        assignment_to_average_attempts.update({question_id: total_attempts / len(people_to_attempts)})
+        
+        #clear the dictionary to have it clearn for the next question
+        people_to_attempts.clear()
+
+get_number_of_attempts()
