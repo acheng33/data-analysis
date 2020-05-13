88e9b1d0f834f3ce0ff7740adac84c0dfacbaed0
Rithvik <rithvik7@illinois.edu>
2020-05-13 02:24:22 -0400

    Updated StudentQuestionAccuracy functionality

--- StudentQuestionAccuracy.py
+++ StudentQuestionAccuracy.py
@@ -18,30 +18,31 @@ db = client.Fall2019Clean
 field_pl_submissions = db.plSubmissions
 
 def StudentQuestionAccuracy():
-    counter = 1
     counts_score = Counter()
     #Find every submission for each question per person
     for document in field_pl_submissions.find():
-        print(counter)
-        counter += 1
-        if "email" in document:
+        if "email" in document and "question_name" in document:
             email = document['email']
-        if "question_name" in document:
             question = document['question_name']
-
+        else:
+            continue
+        #Adds a key email to a value counter. This counter has a key question and a default value of false.
         if (email not in counts_score):
             counts_score[email] = Counter()
+
+        if (question not in counts_score[email]):
             counts_score[email][question] = False
 
         score = document['score']
         #Done to avoid repeating multiple successful submissions
-        if (score is not None and score > 0 and question not in counts_score[email]):
+        if (score is not None and score > 0 and counts_score[email][question] == False):
             counts_score[email][question] = True
 
     fractions_score = Counter()
 
     for email in counts_score:
-        for question, positive_score in counts_score[email].items():
+        for question, is_positive_score in counts_score[email].items():
+            #Adds the question key to the counter.
             if question not in fractions_score:
                 fractions_score[question] = Counter()
                 fractions_score[question]["Numerator"] = 0
@@ -49,16 +50,16 @@ def StudentQuestionAccuracy():
 
             fractions_score[question]["Denominator"] += 1
 
-            if (positive_score):
+            if (is_positive_score):
                 fractions_score[question]["Numerator"] += 1
+
     return fractions_score
 
-'''scores = StudentQuestionAccuracy()
+scores = StudentQuestionAccuracy()
 with open("StudentProbabilities.json", "w") as file:
-    json.dump(scores, file)'''
+    json.dump(scores, file)
 
 def PlotChosenAssignment():
-    data = None
     with open("StudentProbabilities.json", "r") as file:
         data = json.load(file)
     year = sys.argv[1]
@@ -90,8 +91,6 @@ def PlotChosenAssignment():
             is_green_bar = True
 
     bars = plt.bar(x, y, edgecolor='black', color=[colors[elem] for elem in bar_colors])
-
-    # Assigns the average score above each bar
 
     plt.xticks(x, x, rotation=45)
     plt.title("Percentage Of Students Who Got More Than 0 Points in each question")
